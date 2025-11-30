537b0a60cfda473a50ec5cf575b4dd5777ff1bc3
atcovolan <atcovolan@gmail.com>
2025-11-30 14:19:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+produto = "https://br.puma.com/pd/bone-puma-x-kidsuper-baseball/026601.html"
+params = {
+    'color': '01',
+}
+
+def monitor():
+    response = requests.get(produto, params=params)
+    print(response.status_code)
+
+monitor()
