41ddefb31cbe11e7ffa2b8d8278deb45530f683f
atcovolan <atcovolan@gmail.com>
2025-12-04 23:54:17 -0300

    webhook configurado

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import requests
+import time
+import json
+from pathlib import Path
 
-produto = "https://br.puma.com/pd/bone-puma-x-kidsuper-baseball/026601.html"
-params = {
-    'color': '01',
-}
+def load_config():
+    config_path = Path(__file__).resolve().parent / "config.json"
+    with open(config_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+config = load_config()
+
+
+produto = config["produto"]
+webhook_url = config["webhook_url"]
+
+
+def enviar_webhook(sku):
+    data = {
+        "content": f"O produto teve alteração no preço!\n🔗 {produto}"
+    }
+    try:
+        response = requests.post(webhook_url, json=data)
+        if response.status_code == 204:
+            print(f"[🚀] Webhook enviado com sucesso para {sku}")
+        else:
+            print(f"[⚠️] Falha ao enviar webhook: {response.status_code}")
+    except Exception as e:
+        print(f"[⚠️] Erro ao enviar webhook: {e}")
+
+
 
 def monitor():
-    response = requests.get(produto, params=params)
-    print(response.status_code)
-    print(response.text)
+    response = requests.get(produto)
+
+    while True:
+        if '"price":399.99' in response.text:
+            print("Segue preço antigo")
+        else:
+            print("Preço alterado, confirmar!")
+        time.sleep(3)
 
 monitor()
